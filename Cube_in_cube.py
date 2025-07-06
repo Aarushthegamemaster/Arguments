1b8b3f2e49a2225dd3b777dffe869a7395b939ea
Aarushthegamemaster <aarushchandra0604@gmail.com>
2025-07-05 18:36:31 -0700

    Cube_in_Cube

---
+++ Cube_in_cube.py
@@ -0,0 +1,11 @@
+def cube (number):
+    return number*number*number
+
+def divisible_three (number):
+    if number %3 -8:
+        return cube(number)
+    else:
+        return False
+    
+print(divisible_three(9))
+print(divisible_three(4))
